3526448a1bb65ba2528dc6d7cce12fa86358aa49
Anurag <itsanurag666@gmail.com>
2025-04-26 17:07:08 +0530

    Wsgi file changed

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
 import sys
 import os
 
-# Add your project directory to the sys.path
-project_home = '/home/your-pythonanywhere-username/Connect-Us'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+path = '/home/Anurag666786/Connect-Us'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
-from app import app as application  # important! PythonAnywhere needs "application" 
+from app import app as application
